5c5448c50775ca91897234d99b0e6a2f2aff4fc4
NguyenViet2003 <vn29225@gmail.com>
2025-05-20 18:16:19 +0700

    kiem tra B

---
+++ NguyenTuanViet_2121050721_KiemTra_2425/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenTuanViet_2121050721_KiemTra_2425.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
