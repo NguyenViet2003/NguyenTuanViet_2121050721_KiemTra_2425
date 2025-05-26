2812d1b1c54e2f687d29f17c4e44244ac84178e4
NguyenViet2003 <vn29225@gmail.com>
2025-05-26 15:44:00 +0700

    baikiemtra

---
+++ NguyenTuanViet_2121050721_KiemTra_2425/Controllers/Democontroller.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NguyenTuanViet_2121050721_KiemTra_2425.Controllers;
+
+public class Democontroller : Controller
+{
+    public IActionResult DemoABC()
+    {
+        return View();
+    }
+    
+}
+
---
+++ NguyenTuanViet_2121050721_KiemTra_2425/Models/DemoABC.cs
@@ -0,0 +1,10 @@
+namespace NguyenTuanViet_2121050721_KiemTra_2425.Models;
+
+public class DemoABC
+{
+    public string TenA {get; set; }
+    public string TenB { get; set; }
+    public string TenC { get; set; }
+
+
+}
